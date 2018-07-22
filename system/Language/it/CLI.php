be5d02bb82c2b9dac3002621637c97dee76d023a
Chalda Pnuzig <chalda82@gmail.com>
2018-07-22 05:29:40 +0200

    Create CLI.php

---
+++ system/Language/it/CLI.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CLI language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT	MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 3.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+return [
+	'helpUsage'       => 'Utilizzo:',
+	'helpDescription' => 'Descrizione:',
+	'helpOptions'     => 'Opzioni:',
+	'helpArguments'   => 'Argomenti:',
+	'invalidColor'    => 'Colore {0} invalido: {1}.',
+];
