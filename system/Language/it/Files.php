51a3718b8bc80edf1f7e3b8a9c57ee95316c287b
Chalda Pnuzig <chalda82@gmail.com>
2018-07-22 05:50:43 +0200

    Create Files.php

---
+++ system/Language/it/Files.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Files language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT	MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 3.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+return [
+	'fileNotFound'		 => 'File non trovato: {0}',
+	'cannotMove'		 => 'Impossibile spostare il file {0} in {1} ({2})',
+	'invalidFilename'	 => 'Nome file di destinazione mancante o non valido: {0}',
+	'cannotCopy'		 => 'Impossibile copiare in {0} - assicurarsi che la cartella sia scrivibile',
+];
