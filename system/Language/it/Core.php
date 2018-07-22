a91c736b69edeaf9ba7ce01ea59a04c5e15dd143
Chalda Pnuzig <chalda82@gmail.com>
2018-07-22 05:35:21 +0200

    Create Core.php

---
+++ system/Language/it/Core.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Core language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT	MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 3.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+return [
+	'invalidFile'      => 'File non valido: {0}',
+	'copyError'        => 'Si è riscontrato un errore nella sostituzione del file. Controllare che la directory sia scrivibile.',
+	'missingExtension' => 'L\'esensione {0} non è stata caricata.',
+	'noHandlers'       => '{0} deve prevedere almeno un Handler.',
+];
