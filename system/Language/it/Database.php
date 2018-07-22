4c7cc958d05cb20f8bd4e9847624804f0fba7e1c
Chalda Pnuzig <chalda82@gmail.com>
2018-07-22 05:37:30 +0200

    Create Database.php

---
+++ system/Language/it/Database.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Database language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT	MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 3.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+return [
+	'invalidEvent'         => '{0} non è un callback di Model Event.',
+	'invalidArgument'      => 'Devi fornire un {0} valido.',
+	'invalidAllowedFields' => 'I campi consentiti devono essere specificati per il modello: {0}',
+	'emptyDataset'         => 'Non ci sono dati per {0}.',
+];
