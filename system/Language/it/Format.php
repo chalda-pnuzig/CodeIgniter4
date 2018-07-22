52e7b13da0a264557260b92d92b03f55313a6c09
Chalda Pnuzig <chalda82@gmail.com>
2018-07-22 05:54:52 +0200

    Create Format.php

---
+++ system/Language/it/Format.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Format language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT	MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 3.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+return [
+	'invalidJSON'      => 'Impossibile analizzare la stringa json, errore: "{0}".',
+	'missingExtension' => 'È richiesta l\'estensione SimpleXML per formattare l\'XML.',
+];
