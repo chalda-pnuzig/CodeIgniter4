d7213091799df5634c92c45afc47c5ff7d8cc1e7
Chalda Pnuzig <chalda82@gmail.com>
2018-07-22 05:32:02 +0200

    Create Cache.php

---
+++ system/Language/it/Cache.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Cache language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT	MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 3.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+return [
+	'invalidHandlers' => 'La configurazione della cache deve avere un array di $validHandlers.',
+	'noBackup'        => 'La configurazione della cache deve avere impostato un handler ed un backupHandler.',
+	'handlerNotFound' => 'La configurazione della cache ha un handler o un backupHandler invalido o non impostato.',
+];
